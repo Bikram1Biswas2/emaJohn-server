111aa983ea437a373264a553b24b0c57ccda6833
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2024-12-18 12:16:21 +0530

    set page limit and skip

--- index.js
+++ index.js
@@ -29,7 +29,13 @@ async function run() {
     const productCollection = client.db('emaJohnDBNew').collection('products');
 
     app.get('/products', async(req, res) => {
-        const result = await productCollection.find().toArray();
+      const page = parseInt(req.query.page)
+      const size = parseInt(req.query.size)
+      console.log(page,size);
+        const result = await productCollection.find()
+        .skip(page*size)
+        .limit(size)
+        .toArray();
         res.send(result);
     })
 
